116c06fbf50315d6af96e51e7c547d9adff22f05
Samuel <samuelh08.shm@gmail.com>
2022-10-28 18:05:09 -0500

    Add database configuration with sequelize

--- app/config.js
+++ app/config.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT,
+  database: {
+    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+  },
 };
 
 module.exports = config;
