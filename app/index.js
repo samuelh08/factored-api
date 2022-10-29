ba5f8ce1dcbde6392a21a9d32f901ba032610886
Samuel <samuelh08.shm@gmail.com>
2022-10-29 12:01:01 -0500

    Add cors

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const api = require('./api');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', api);
